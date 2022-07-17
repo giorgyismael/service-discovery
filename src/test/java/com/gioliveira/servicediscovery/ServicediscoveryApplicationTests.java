ffbf8a693b33d7980ae34eeb0792369e403f65c1
Giorgy Oliveira <giorgy.oliveira@neogrid.com>
2022-07-16 22:05:38 -0300

    first commit

---
+++ src/test/java/com/gioliveira/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gioliveira.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
